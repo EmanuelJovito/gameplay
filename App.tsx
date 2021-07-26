94f690ff14eed6d14e72b66bcbd13c56edab4ddc
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-07-26 10:17:20 -0300

    feat: Adicionando linear gradiente no Background da aplicação

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhan
 import AppLoading from 'expo-app-loading'
 
 import { SignIn } from './src/screens/SignIn';
+import { Background } from './src/components/Background';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +23,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
@@ -30,6 +31,6 @@ export default function App() {
       />
 
       <SignIn />
-    </>
+    </Background>
   )
 }
